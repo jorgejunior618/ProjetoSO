64e0254033269c3f3433f6f8510e7a86a14b8c99
jorgejunior618 <jorgejunior618@gmail.com>
2022-03-22 12:20:29 -0300

    Adição de semáforos e desenvolvimento do EMpacotador (Minimo)

--- src/trem/carga/Empacotador.java
+++ src/trem/carga/Empacotador.java
@@ -1,5 +1,44 @@
 package trem.carga;
 
-public class Empacotador {
-
+public class Empacotador extends Thread {
+	public int id;
+	public String nome;
+	public int tempoEmpacotamento;
+	
+	public Empacotador(int id, String nome, int te) {
+		super(nome);
+		this.id = id;
+		this.tempoEmpacotamento = te;
+		this.nome = nome;
+	}
+	
+	private void empacotar() {
+		long inicio = System.currentTimeMillis();
+		while(System.currentTimeMillis() - inicio  < (long) tempoEmpacotamento * 1000) {
+		}
+		System.out.println("Terminei");
+	}
+	
+	private void inserirPacote() {
+		Main.cargaDeposito += 1;
+	}
+	
+	public void run() {
+		while(true) {
+			empacotar();
+			try {
+				Main.empty.acquire();
+				Main.mutex.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			inserirPacote();
+			Main.mutex.release();
+			Main.full.release();
+			
+			System.out.println(String.format("Carga do Deposito: %d", Main.cargaDeposito));
+			
+		}
+	}
+	
 }
--- src/trem/carga/Main.java
+++ src/trem/carga/Main.java
@@ -1,6 +1,16 @@
 package trem.carga;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
+	private static Empacotador[] empacotadores = new Empacotador[10];
+	private static Trem tremDeCarga = new Trem();
+	
+	public static Semaphore empty = new Semaphore(3);
+	public static Semaphore full = new Semaphore(0);
+	public static Semaphore mutex = new Semaphore(1);
+	
+	public static int cargaDeposito = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
