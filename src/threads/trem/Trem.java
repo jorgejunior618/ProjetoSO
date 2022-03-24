8ad0fbe496e1b1a405e0126f9f06e101bcc382ed
Raphael <101840118+Atabashi@users.noreply.github.com>
2022-03-24 10:19:51 -0300

    Update Trem.java

--- src/threads/trem/Trem.java
+++ src/threads/trem/Trem.java
@@ -1,5 +1,39 @@
 package threads.trem;
 
-public class Trem {
+public class Trem extends Thread{
+	public int id;
+	public int numPacotes;
+	public int tempoTransporte;
 
+	public Trem(int id, int np, int tt) {
+		this.id = id;
+		this.numPacotes = np;
+		this.tempoTransporte = tt;
+	}
+	
+	private void transportar() {
+		long inicio = System.currentTimeMillis();
+		while(System.currentTimeMillis() - inicio  < (long) tempoTransporte * 5000) {
+		}
+		System.out.println(String.format("Trem terminou o transporte"));
+	}
+
+	private void descarregarPacote() {
+		Main.cargaTrem = 0;
+	}
+
+	public void run() {
+		while(true) {
+			transportar();
+			try {
+				Main.full.release();
+				Main.mutex.release();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			descarregarPacote();
+			Main.full.acquire();
+			Main.mutex.acquire();
+		}
+	}
 }
