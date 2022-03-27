a76b2e4b011101d5c8c482ed51d39695156b9c54
João Augusto <61090559+joaosouzac@users.noreply.github.com>
2022-03-27 20:45:19 -0300

    Integração Trem e Empacotador

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,2 @@
-module TremCarga {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens threads.trem to javafx.graphics, javafx.fxml;
+module projeto_so {
 }
