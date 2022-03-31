831fb9f1b716687dfd28b434e796d2286436b58d
jorgejunior618 <jorgejunior618@gmail.com>
2022-03-31 09:31:15 -0300

    Alteração de estilização

--- src/threads/trem/Main.java
+++ src/threads/trem/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MenuInicial.fxml"));
-			Scene scene = new Scene(root, 300, 300);
+			Scene scene = new Scene(root, 400, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
