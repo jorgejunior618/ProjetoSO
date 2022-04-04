de155cd67b5961cd9d445f3ca0cb2228097afbd9
Raphael <101840118+Atabashi@users.noreply.github.com>
2022-04-03 21:59:44 -0300

    Update Main.java

--- src/threads/trem/Main.java
+++ src/threads/trem/Main.java
@@ -5,27 +5,40 @@ import java.util.concurrent.Semaphore;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	private static Empacotador[] empacotadores = new Empacotador[10];
-	private static Trem tremDeCarga = new Trem();
+	public static Empacotador[] empacotadores = new Empacotador[10];
+	public static Trem tremDeCarga;
+	
+	public static int qtdEmpacotadores;
+	public static int tempoEmpacotamentoInicial;
+	public static int tempoViagemInicial;
+	
+	public static ModoJogo modoJogo;
 
-	public static Semaphore empty = new Semaphore(3);
+	public static int cargaMaximaDeposito;	// M
+	public static int cargaMaximaVagao;		// N
+	public static Semaphore empty;
 	public static Semaphore full = new Semaphore(0);
+
 	public static Semaphore mutex = new Semaphore(1);
 
 	public static int cargaDeposito = 0;
-	
+		
+	public static int qtmoedas = 0;			//Variavel que guarda as moedas atuais.
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("TelaEstacao.fxml"));
-			Scene scene = new Scene(root,900,460);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MenuInicial.fxml"));
+			Scene scene = new Scene(root, 400, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
