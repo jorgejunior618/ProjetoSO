611373c1d9f4a096898d53e63df5641dccb46760
jorgejunior618 <jorgejunior618@gmail.com>
2022-04-08 14:03:13 -0300

    Adiçao de novas Imagens e ajustes de estilização

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,8 @@
-module projeto_so {
-  // requires javafx.controls;
-	// requires javafx.fxml;
-	// requires javafx.base;
-	// requires javafx.graphics;
-	
-	// opens threads.trem to javafx.graphics, javafx.fxml;
+module TremCarga {
+   requires javafx.controls;
+   requires javafx.fxml;
+   requires javafx.base;
+   requires javafx.graphics;
+
+   opens threads.trem to javafx.graphics, javafx.fxml;
 }
