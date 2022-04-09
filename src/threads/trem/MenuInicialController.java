c13fe2b02d950362532d86aedc0400cba2dd6777
jorgejunior618 <jorgejunior618@gmail.com>
2022-04-08 21:23:01 -0300

    remoção de importações desnecessárias

--- src/threads/trem/MenuInicialController.java
+++ src/threads/trem/MenuInicialController.java
@@ -5,22 +5,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.Semaphore;
 
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.SingleSelectionModel;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -66,17 +56,17 @@ public class MenuInicialController implements Initializable {
         	Scene scene = new Scene(root, 1024, 512);
         	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-            Stage stage = new Stage();
-            stage.setTitle("Projeto: Estação");
-            stage.setScene(scene);
-            stage.setResizable(false);
+            Stage telaEstacaoStage = new Stage();
+            telaEstacaoStage.setTitle("Projeto: Estação");
+            telaEstacaoStage.setScene(scene);
+            telaEstacaoStage.setResizable(false);
 
-            stage.setOnCloseRequest(closeEvent -> {
+            telaEstacaoStage.setOnCloseRequest(closeEvent -> {
             	closeEvent.consume();
-				Main.fecharJogo(stage);
+				Main.fecharJogo(telaEstacaoStage);
 			});
             
-            stage.show();
+            telaEstacaoStage.show();
             sair(event);
         }
         catch (IOException e) {
@@ -95,17 +85,17 @@ public class MenuInicialController implements Initializable {
         	Scene scene = new Scene(root, 500, 420);
         	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-            Stage stage = new Stage();
-            stage.setTitle("Projeto: Estação");
-            stage.setScene(scene);
-            stage.setResizable(false);
+            Stage menuEstacaoStage = new Stage();
+            menuEstacaoStage.setTitle("Projeto: Estação");
+            menuEstacaoStage.setScene(scene);
+            menuEstacaoStage.setResizable(false);
             
-            stage.setOnCloseRequest(closeEvent -> {
+            menuEstacaoStage.setOnCloseRequest(closeEvent -> {
             	closeEvent.consume();
-				Main.fecharJogo(stage);
+				Main.fecharJogo(menuEstacaoStage);
 			});
             
-            stage.show();
+            menuEstacaoStage.show();
             sair(event);
         }
         catch (IOException e) {
@@ -117,25 +107,7 @@ public class MenuInicialController implements Initializable {
 	private void sair(ActionEvent event) {
 		stage = (Stage) telaMenuInicial.getScene().getWindow();
 		
-		boolean empacotadoresAtivos = true;
-		boolean tremAtivo = true;
-		Main.encerrarThreads = true;
-		
-		System.out.println("Encerrando threads");
-		while (empacotadoresAtivos || tremAtivo) {
-			if(tremAtivo) {
-				tremAtivo = (Main.tremDeCarga != null && Main.tremDeCarga.isAlive());
-			}
-			if(empacotadoresAtivos) {
-				for (int i = 0; i < 10; i++) {
-					empacotadoresAtivos = (Main.empacotadores[i] != null && Main.empacotadores[i].isAlive());
-					if (empacotadoresAtivos) break;
-				}
-			}
-		}
-		System.out.println("Threads encerradas");
-		
-		stage.close();
+		Main.fecharJogo(stage);
 	}
 
 	@Override
